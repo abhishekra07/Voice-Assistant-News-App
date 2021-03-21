398e8ee6b346a87a4c660c055de4c354bd26e97c
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2021-03-21 15:25:42 +0530

    added alanBtn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanAPIKey = ALAN_API_KEY;
 
 const App = () => {
+
+  useEffect( () => {
+    alanBtn({
+      key: alanAPIKey,
+      onCommand: ({ command }) => {
+        if(command === 'testCommand'){
+          alert('testCommand is executes');
+        }
+      }
+    })
+  }, [])
+
   return (
     <div >
       Alan AI News Application
